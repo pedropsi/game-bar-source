c4c84702d410c95da217759e8900b2207892e518
pedropsi <pedropsi.github.io.at.github@gmail.com>
2019-07-26 17:23:11 +0100

    Revert "Force Async"
    This reverts commit 794ec1e5e0c0f7c83769697ccd172b3510e81698.

--- codes/game/puzzlescript-embed.js
+++ codes/game/puzzlescript-embed.js
@@ -1,7 +1,7 @@
 // Add the game container to the page
 PrependElement('<div class="game-container"><div id="puzzlescript-game" class="game"><canvas id="gameCanvas"></canvas></div></div>',"body");
 
-// Load all Puzzlescript Modules & Cartridge
+// Load the puzzlescrpt engine
 var puzzlescriptModules=[
 /*Puzzlescript modules*/
 "globalVariables",
@@ -17,15 +17,61 @@ var puzzlescriptModules=[
 "parser",
 "compiler",
 "inputoutput",
-
-/*Modified touch module*/
 "mobile",
-
 /*Extras*/
-"data-game-extras",
-
-/*Load game cartridge*/
-"data-game-loader"
+"data-game-extras"
 ]
 
 puzzlescriptModules.map(LoaderInFolder("codes/game/modules"));
+
+// Load the game
+(function()
+{
+	function embed(element, id, config)
+	{
+		var canvas=document.getElementById("gameCanvas");
+		canvas.onmousedown = function(){
+			canvas.onmousedown = null;
+			var gestureHandler = Mobile.enable(true);
+		}
+		
+		return load_game(element, id);
+	}
+
+	function load_game(element, id){
+		var githubURL = 'https://api.github.com/gists/' + id;
+		var githubHTTPClient = new XMLHttpRequest();
+
+		githubHTTPClient.open('GET', githubURL);
+		githubHTTPClient.onreadystatechange = function () {
+			if (githubHTTPClient.readyState != 4) {
+				return;
+			}
+			var result = JSON.parse(githubHTTPClient.responseText);
+			if (githubHTTPClient.status === 403) {
+				console.log("forbidden");
+				displayError(403,result.message);
+			} else if (githubHTTPClient.status !== 200 && githubHTTPClient.status !== 201) {
+				console.log("not ok, not created");
+				displayError(200,"HTTP Error " + githubHTTPClient.status + ' - ' + githubHTTPClient.statusText);
+			} else {
+				console.log("loaded");
+
+				var result = JSON.parse(githubHTTPClient.responseText);
+				var code = result["files"]["script.txt"]["content"];
+				compile(["restart"], code);
+				
+				window.scrollTo(0,0);
+				/////////////////////////////////////////////////////////////////////////////////////
+				// Game bar
+				if(typeof AddGameBar!=="undefined")
+					AddGameBar();
+			}
+		}
+		githubHTTPClient.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		githubHTTPClient.send();
+	}
+
+	window.PuzzleScript = window.PuzzleScript || { embed: embed };
+
+})();
