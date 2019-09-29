b7bc133f84a45f1eabe822b107d60d1dde7fa5ca
pedropsi <pedropsi.github.io.at.github@gmail.com>
2019-09-29 13:57:04 +0100

    Focus Spotlight

--- codes/game/modules/data-game-extras.js
+++ codes/game/modules/data-game-extras.js
@@ -79,7 +79,7 @@ function AddGameBar(targetIDsel){
 function GameFocus(DP){
 	document.activeElement.blur();
 	window.Mobile.GestureHandler.prototype.fakeCanvasFocus();
-	FocusElement(gameSelector);
+	FocusSpotlight(gameSelector);
 };
 
 function UndoAndFocus(){
