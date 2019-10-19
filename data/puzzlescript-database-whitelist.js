4c045f8b7e9b50e543680ca77a4254fa053fc72b
pedropsi <pedropsi.github.io.at.github@gmail.com>
2019-10-19 08:35:57 +0100

    u

--- data/puzzlescript-database-whitelist.js
+++ data/puzzlescript-database-whitelist.js
@@ -4,7 +4,7 @@ function Whitelist(){ //Sort descending by expected number of submissions
 		/^https\:\/\/(www\.)?puzzlescript\.net\/play\.html\?p\=.*/,
 		/^https\:\/\/(www\.)?puzzlescript\.net\/editor\.html\?hack\=.*/,
 		/^https\:\/\/[^\#\^\~\\\/\|\.\:\;\,\s\?\=\}\{\[\]\&\'\"\@\!]*\.github\.io\/.*/i,
-		/^http\:\/\/htmlpreview\.github\.io\/\?https\:\/\/raw\.githubusercontent\.com\/ThatScar\/PuzzleScript\/.*/i,
+		/^http\:\/\/htmlpreview\.github\.io\/\?https\:\/\/raw\.githubusercontent\.com\/.*/i,
 		/^https\:\/\/[^\#\^\~\\\/\|\.\:\;\,\s\?\=\}\{\[\]\&\'\"\@\!]*\.github\.io\/.*/i,
 		/^https\:\/\/(www\.)?increpare\.com\/.*/,
 		/^https?\:\/\/(www\.)?draknek\.org\/.*/,
@@ -21,7 +21,8 @@ function Whitelist(){ //Sort descending by expected number of submissions
 var knownAuthors={
 	"unblock":"NOA Cube Studio",
 	"Rose":"Jared Piers",
-	"Im too far gone":"Jack Lance"
+	"Im too far gone":"Jack Lance",
+	"skeleton assembler 2":"Ethan Clark"
 }
 
 function KnownAuthor(title){
