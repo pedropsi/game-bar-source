c0c8c5599b6f55204aa3fef0e91ff243d972cf4b
pedropsi <pedropsi.github.io.at.github@gmail.com>
2019-10-29 12:17:58 +0000

    Update cacher.js

--- codes/cacher.js
+++ codes/cacher.js
@@ -1,2 +1,16 @@
 console.log("I'm a service worker");
 
+// Install 
+self.addEventListener('install', function(event) {
+    console.log('install');
+});
+
+// Activate 
+self.addEventListener('activate', function(event) {
+    console.log('activate');
+});
+
+// Listen for network requests from the main document
+self.addEventListener('fetch', function(event) {
+    console.log('fetch');
+});
