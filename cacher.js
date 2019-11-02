acb0a2794064a2735325dfa5d0df7466db980e7e
pedropsi <pedropsi.github.io.at.github@gmail.com>
2019-11-02 23:56:34 +0000

    Update cacher.js

--- cacher.js
+++ cacher.js
@@ -15,7 +15,6 @@ self.addEventListener('install',function(event){
 	event.waitUntil(
 		caches.open('v4').then(function(cache){
 			console.log("cached!");
-			A();
 			return cache.addAll([
 				'./cacher.js',
 				
